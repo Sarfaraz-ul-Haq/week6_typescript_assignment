b9dcdb953c50f4c21b6456fe0778ce49be7bb917
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2023-12-17 19:37:34 +0500

    commit after moving JS files to dist folder

--- exercise02.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-//Write a program that calculates the percentage.
-Object.defineProperty(exports, "__esModule", { value: true });
-function calculatePercentage(part, whole) {
-    let percentage = (part / whole) * 100;
-    console.log(percentage, "%");
-}
-calculatePercentage(75, 100); //prints 75
