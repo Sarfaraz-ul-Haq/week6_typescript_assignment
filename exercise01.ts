eed818e57cd2b26e38a0d6306ebb7a7f53241ead
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 16:15:12 +0500

    change comments

--- exercise01.ts
+++ exercise01.ts
@@ -1,21 +1,21 @@
-//Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
+// Write a program to convert the temperature from Celsius to Fahrenheit and vice verse.
 
-// //Make function to convert C to F
-//F = (9 / 5) * C + 32;
+// function to convert C to F
+// F = (9 / 5) * C + 32;
 
 function toFahrenheit(tempCelsius: number) {
   let F = (9 / 5) * tempCelsius + 32;
   console.log(Math.round(F));
 }
 
-//Make function to convert F to C
-//C = (5/9) * (F - 32)
+// function to convert F to C
+// C = (5/9) * (F - 32)
 function toCelcius(tempFahrenheit: number) {
   let C = (5 / 9) * (tempFahrenheit - 32);
   console.log(Math.round(C));
 }
 
-//Now using these functions
+// Now using these functions
 let tempInFahrenheit = 95;
 let tempInCelcius = 20;
 
