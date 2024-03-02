1ebdb59e5c320277e5cc75792770372d3e6ae393
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 17:18:57 +0500

    program updated using function approach

--- exercise05.ts
+++ exercise05.ts
@@ -1,11 +1,15 @@
-//Create a program that determines the category of a user-provided age. If the age is between 0 and 12, print "Child." If it's between 13 and 19, print "Teenager." Otherwise, print "Adult."
+// Create a program that determines the category of a user-provided age. If the age is between 0 and 12, print "Child." If it's between 13 and 19, print "Teenager." Otherwise, print "Adult."
 
-let userAge = 20;
-
-if (userAge > 0 && userAge <= 12) {
-  console.log("Child");
-} else if (userAge >= 13 && userAge <= 19) {
-  console.log("Teenager");
-} else {
-  console.log("Adult");
+function ageCategorizer(age: number) {
+  if (age >= 0 && age < 12) {
+    console.log("Child");
+  } else if (age >= 13 && age < 19) {
+    console.log("Teenager");
+  } else {
+    console.log("Adult");
+  }
 }
+
+ageCategorizer(10); // prints child
+ageCategorizer(15); // prints teenager
+ageCategorizer(12); // prints adult
