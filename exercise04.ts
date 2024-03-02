34c850af0af251a5fd9754677ad44bc0b99fdb89
Sarfaraz <isarfaraz80@gmail.com>
2024-03-02 17:15:27 +0500

    edit console.log

--- exercise04.ts
+++ exercise04.ts
@@ -4,15 +4,15 @@ function calculateDiscount(productPrice: number): void {
   let discount;
   if (productPrice <= 100) {
     discount = (productPrice * 5) / 100;
-    console.log(`${discount}$`);
+    console.log(`5% discount: $${discount}`);
   } else {
     discount = (productPrice * 10) / 100;
-    console.log(`${discount}$`);
+    console.log(`10% discount: $${discount}`);
   }
 }
 //Now using "calculateDiscount" function
 let priceOfProduct = 1000;
-calculateDiscount(priceOfProduct); // prints 100$
+calculateDiscount(priceOfProduct); // 10% discount: $100
 
 priceOfProduct = 50;
-calculateDiscount(priceOfProduct); //prints 2.5$
+calculateDiscount(priceOfProduct); // 5% discount: $2.5
