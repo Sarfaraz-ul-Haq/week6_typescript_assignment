bc1b4d2cf331f73c44cd28abd93413ebc15ec585
Sarfaraz <isarfaraz80@gmail.com>
2024-03-03 10:39:39 +0500

    improve code

--- exercise10.ts
+++ exercise10.ts
@@ -1,24 +1,22 @@
 // //Write a program that takes the number of units consumed by a user. If it is greater than 100 then add 10% tax, if it is greater than 200 then add 15% of tax so on up to if greater than 500 then add 25% of tax
 
-function calculateTax(unitsConsumedByUser: number): void {
+function calculateTax(unitsConsumedByUser: number): string {
   let tax: string;
 
   if (unitsConsumedByUser > 100 && unitsConsumedByUser <= 200) {
     tax = "10%";
-    console.log(tax);
+    return `Tax applied: ${tax}`;
   } else if (unitsConsumedByUser > 200 && unitsConsumedByUser <= 500) {
     tax = "15%";
-    console.log(tax);
+    return `Tax applied: ${tax}`;
   } else if (unitsConsumedByUser > 500) {
     tax = "25%";
-    console.log(tax);
+    return `Tax applied: ${tax}`;
   } else {
-    console.log(
-      "There is no tax when consumed units are less than or equal to 100"
-    );
+    return "There is no tax when consumed units are less than or equal to 100";
   }
 }
 
-//Now using the calculateTax function
-let unitsConsumedByUser = 250;
-calculateTax(unitsConsumedByUser);
+const unitsConsumedByUser = 250;
+const calculatedTax = calculateTax(unitsConsumedByUser);
+console.log(calculatedTax);
